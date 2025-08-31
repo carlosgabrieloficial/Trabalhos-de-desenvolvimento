cae76179c6255ccc85442c6bccb7589faae29399
Carlos Gabriel Sousa <carlos.desenvolvedor.dev.@gmail.com>
2025-08-31 12:29:40 -0300

    V 1.1.2

--- CLINICA VIDA+.py
+++ CLINICA VIDA+.py
@@ -3,7 +3,8 @@
 escolha = 0
 while escolha != 5:
 
-    print("""
+    print("""   
+    === Clínica Vida+ ===
     [1] Cadastrar Paciente
     [2] Ver estatísticas
     [3] Buscar pacientes
@@ -14,7 +15,7 @@ while escolha != 5:
     escolha = int(input("Por Favor, escolha uma das opção : "))
    
     if escolha == 1   :
-        print("Opção [1] Cadastrar Paciente ")
+        print("Cadastrar Paciente ")
 
     elif escolha == 2 :
         print("Opção [2] Ver estatísticas ")
